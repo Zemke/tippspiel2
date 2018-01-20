127e60e9d4cb07a2b23ff6dfaf4fa4d5b84e282d
Zemke <flzemke@gmail.com>
2018-01-20 23:30:38 +0100

    Set reserved user table name properly
    I'd previously thought that would've been achieved using the Entity
    annotation—d’oh!

--- tippspiel2-spring/src/main/kotlin/com/github/zemke/tippspiel2/entity/User.kt
+++ tippspiel2-spring/src/main/kotlin/com/github/zemke/tippspiel2/entity/User.kt
@@ -7,10 +7,12 @@ import java.sql.Timestamp
 import javax.persistence.Embedded
 import javax.persistence.Entity
 import javax.persistence.Id
+import javax.persistence.Table
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
-@Entity(name = "\"user\"")
+@Entity
+@Table(name = "\"user\"")
 data class User(
 
         @Id val id: Long?,
