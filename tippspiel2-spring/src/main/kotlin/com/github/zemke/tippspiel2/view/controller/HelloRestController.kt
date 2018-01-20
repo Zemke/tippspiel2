2d8b474e19a4a35a260ee6b1b4fca0e916222dbb
Zemke <flzemke@gmail.com>
2018-01-20 16:11:49 +0100

    A more REST-like path

--- tippspiel2-spring/src/main/kotlin/com/github/zemke/tippspiel2/view/controller/HelloRestController.kt
+++ tippspiel2-spring/src/main/kotlin/com/github/zemke/tippspiel2/view/controller/HelloRestController.kt
@@ -3,13 +3,15 @@ package com.github.zemke.tippspiel2.view.controller
 import com.github.zemke.tippspiel2.view.model.HelloDto
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
 @RestController
+@RequestMapping("/api/hellos")
 class HelloRestController() {
 
-    @GetMapping("/hello")
+    @GetMapping("")
     fun helloKotlinJson(): ResponseEntity<List<HelloDto>> {
         return ResponseEntity.ok(Collections.singletonList(HelloDto("world")));
     }
