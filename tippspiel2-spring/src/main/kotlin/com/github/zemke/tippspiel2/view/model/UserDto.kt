685c876c7ba95f024e240d926c5189ed5b2df464
Zemke <flzemke@gmail.com>
2018-01-20 23:32:23 +0100

    Add JsonProperty annotations to UserDto
    Using this avoids necessity of an empty default constructor.

--- tippspiel2-spring/src/main/kotlin/com/github/zemke/tippspiel2/view/model/UserDto.kt
+++ tippspiel2-spring/src/main/kotlin/com/github/zemke/tippspiel2/view/model/UserDto.kt
@@ -1,14 +1,15 @@
 package com.github.zemke.tippspiel2.view.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.github.zemke.tippspiel2.entity.User
 
 
 data class UserDto(
 
-        val id: Long,
-        val firstName: String,
-        val lastName: String,
-        val email: String
+        @JsonProperty("id") val id: Long,
+        @JsonProperty("firstName") val firstName: String,
+        @JsonProperty("lastName") val lastName: String,
+        @JsonProperty("email") val email: String
 ) {
     companion object {
 
