ba6807c5370946323fbf8942c2fd217125d0c2ba
Zemke <flzemke@gmail.com>
2018-01-20 23:31:15 +0100

    Fix JwtUser.kt properties

--- tippspiel2-spring/src/main/kotlin/com/github/zemke/tippspiel2/security/JwtUser.kt
+++ tippspiel2-spring/src/main/kotlin/com/github/zemke/tippspiel2/security/JwtUser.kt
@@ -1,15 +1,16 @@
 package com.github.zemke.tippspiel2.security
 
 import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 import java.util.*
 
 class JwtUser(
         val id: Long, val firstName: String, val lastName: String,
-        val email: String, password: String, val lastPasswordResetDate: Date?) : UserDetails {
+        val email: String, val plainPassword: String, val lastPasswordResetDate: Date?) : UserDetails {
 
-    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
-        TODO("not implemented")
+    override fun getAuthorities(): Collection<GrantedAuthority> {
+        return listOf<GrantedAuthority>(SimpleGrantedAuthority("USER"))
     }
 
     override fun isEnabled(): Boolean = true
@@ -18,7 +19,7 @@ class JwtUser(
 
     override fun isCredentialsNonExpired(): Boolean = true
 
-    override fun getPassword(): String = password
+    override fun getPassword(): String = plainPassword
 
     override fun isAccountNonExpired(): Boolean = true
 
