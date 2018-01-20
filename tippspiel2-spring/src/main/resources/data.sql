24871df89d1a15951c7b56776bb4456faf9538e5
Zemke <flzemke@gmail.com>
2018-01-20 23:30:01 +0100

    Add initialization script which adds a user
    Password is "hamburger".

---
+++ tippspiel2-spring/src/main/resources/data.sql
@@ -0,0 +1,18 @@
+INSERT INTO "user" (
+  ID,
+  FIRST_NAME,
+  LAST_NAME,
+  EMAIL,
+  "PASSWORD",
+  LAST_PASSWORD_RESET
+)
+VALUES (
+  1,
+  'Florian',
+  'Zemke',
+  'Florian.Zemke@btc-ag.com',
+  '$2a$10$NBVm8mNMr87fbRRJGz3XJu971Ro24Mwg2SB8zVUxh0IZp6xnz30GW',
+  sysdate
+);
+
+
