25080d63ec74d6771d713f2b8c725583b0cffc7d
Zemke <flzemke@gmail.com>
2018-03-19 19:21:53 +0100

    GH-2 UI Layout and links
    Closes GH-2

--- tippspiel2-ember/app/router.js
+++ tippspiel2-ember/app/router.js
@@ -7,6 +7,10 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('standings');
+  this.route('games');
+  this.route('sign-up');
+  this.route('sign-in');
 });
 
 export default Router;
