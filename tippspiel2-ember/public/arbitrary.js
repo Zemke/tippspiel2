f80c5696a55a5026f2400df990dbed7f5c01f922
Zemke <flzemke@gmail.com>
2018-03-20 11:10:35 +0100

    GH-2 Make nav work for mobile users

---
+++ tippspiel2-ember/public/arbitrary.js
@@ -0,0 +1,25 @@
+$(document).ready(() => {
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    if ($navbarBurgers.length < 1) {
+      return;
+    }
+
+    function toggle($el, $target) {
+      $el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+    }
+
+    $navbarBurgers.forEach(function ($el) {
+      const $target = document.getElementById($el.dataset.target);
+
+      $el.addEventListener('click', function () {
+        toggle($el, $target);
+      });
+
+      $target.addEventListener('click', function () {
+        toggle($el, $target);
+      });
+    });
+  }
+);
