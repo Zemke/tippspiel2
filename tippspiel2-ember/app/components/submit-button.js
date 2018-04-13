86e519f1a28a5d7bb842a2bc45a05b695f61b948
Zemke <flzemke@gmail.com>
2018-04-13 21:41:48 +0200

    GH-13 Add the actual submit action

--- tippspiel2-ember/app/components/submit-button.js
+++ tippspiel2-ember/app/components/submit-button.js
@@ -1,10 +1,13 @@
 import {computed} from '@ember/object';
 import Component from '@ember/component';
 
-// TODO Implement action
-
 export default Component.extend({
   disabled: computed('model.validations.isInvalid', 'model.isSaving', function () {
     return this.get('model.validations.isInvalid') || this.get('model.isSaving')
-  })
+  }),
+  actions: {
+    submit() {
+      this.get('submit')();
+    }
+  }
 });
