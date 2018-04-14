6cac8c82b7b035eba129aecff4df07a9821d8f8b
Zemke <flzemke@gmail.com>
2018-04-14 21:10:14 +0200

    Split host into host and namespace

--- tippspiel2-ember/app/adapters/application.js
+++ tippspiel2-ember/app/adapters/application.js
@@ -1,5 +1,6 @@
 import RESTAdapter from 'ember-data/adapters/rest';
 
 export default RESTAdapter.extend({
-  host: 'http://0.0.0.0:8080/api'
+  host: 'http://0.0.0.0:8080',
+  namespace: 'api'
 });
