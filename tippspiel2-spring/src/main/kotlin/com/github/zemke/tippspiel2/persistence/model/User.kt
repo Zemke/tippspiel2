51d45e6329912750ef7add17d834be8c7424477e
Zemke <flzemke@gmail.com>
2018-04-15 02:40:33 +0200

    Add unique constraint to user email addresses

--- tippspiel2-spring/src/main/kotlin/com/github/zemke/tippspiel2/persistence/model/User.kt
+++ tippspiel2-spring/src/main/kotlin/com/github/zemke/tippspiel2/persistence/model/User.kt
@@ -3,6 +3,7 @@ package com.github.zemke.tippspiel2.persistence.model
 import com.github.zemke.tippspiel2.persistence.model.embeddable.FullName
 import org.hibernate.annotations.CreationTimestamp
 import java.sql.Timestamp
+import javax.persistence.Column
 import javax.persistence.Embedded
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
@@ -19,7 +20,7 @@ data class User(
 
         @Id @GeneratedValue val id: Long?,
         @Embedded val fullName: FullName,
-        @NotNull @Email val email: String,
+        @Column(unique = true) @NotNull @Email val email: String,
         @Size(min = 60, max = 60) val password: String,
         @NotNull @CreationTimestamp val lastPasswordReset: Timestamp?,
         @ManyToMany(mappedBy = "users") val communities: List<Community>
