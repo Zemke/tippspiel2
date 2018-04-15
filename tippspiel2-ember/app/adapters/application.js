3bb0c4e8183c35ea0df999b07cd313b6b781c3cd
Zemke <flzemke@gmail.com>
2018-04-15 14:38:27 +0200

    GH-13 Get token from service

--- tippspiel2-ember/app/adapters/application.js
+++ tippspiel2-ember/app/adapters/application.js
@@ -1,7 +1,9 @@
 import RESTAdapter from 'ember-data/adapters/rest';
 import {computed} from '@ember/object';
+import {inject} from '@ember/service';
 
 export default RESTAdapter.extend({
+  auth: inject(),
   host: 'http://0.0.0.0:8080',
   namespace: 'api',
   handleResponse(status, headers, payload, requestData) {
@@ -10,9 +12,8 @@ export default RESTAdapter.extend({
       : payload;
   },
   headers: computed(function () {
-    const authTokenFromLocalStorage = localStorage.getItem('auth-token');
-    return authTokenFromLocalStorage != null
-      ? {Authorization: 'Bearer ' + authTokenFromLocalStorage}
+    return this.get('auth').token != null
+      ? {Authorization: 'Bearer ' + this.get('auth').token}
       : {};
   }).volatile()
 });
--- tippspiel2-ember/app/services/auth.js
+++ tippspiel2-ember/app/services/auth.js
@@ -3,22 +3,26 @@ import Service from '@ember/service';
 export default Service.extend({
   authenticated: null,
   user: null,
+  token: null,
   init() {
     this._super(...arguments);
 
     const authToken = localStorage.getItem('auth-token');
     if (authToken != null) {
+      this.set('token', authToken);
       this.set('authenticated', true);
       this.set('user', this.get('parseTokenPayload')(authToken));
     }
   },
   signIn(authToken) {
     localStorage.setItem('auth-token', authToken);
+    this.set('token', authToken);
     this.set('authenticated', true);
     this.set('user', this.get('parseTokenPayload')(authToken));
   },
   signOut() {
     localStorage.removeItem('auth-token');
+    this.set('token', null);
     this.set('authenticated', false);
     this.set('user', null);
   },
