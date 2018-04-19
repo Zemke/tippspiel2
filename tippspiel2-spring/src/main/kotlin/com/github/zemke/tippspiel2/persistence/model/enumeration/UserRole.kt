2138b3dedea5e52fb5d304ba763b67914884ae29
Zemke <flzemke@gmail.com>
2018-04-19 23:38:52 +0200

    GH-9 Write integration test
    Closes GH-9

--- tippspiel2-spring/src/main/kotlin/com/github/zemke/tippspiel2/persistence/model/enumeration/UserRole.kt
+++ tippspiel2-spring/src/main/kotlin/com/github/zemke/tippspiel2/persistence/model/enumeration/UserRole.kt
@@ -2,5 +2,7 @@ package com.github.zemke.tippspiel2.persistence.model.enumeration
 
 enum class UserRole {
     ROLE_USER,
-    ROLE_ADMIN
+    ROLE_ADMIN;
+
+    fun unPrefixed(): String = this.name.substring(5)
 }
---
+++ tippspiel2-spring/src/test/kotlin/com/github/zemke/tippspiel2/view/controller/HelloRestControllerTest.kt
@@ -0,0 +1,58 @@
+package com.github.zemke.tippspiel2.view.controller
+
+import com.github.zemke.tippspiel2.SpringBootTippspiel2Application
+import com.github.zemke.tippspiel2.core.config.WebSecurityConfig
+import com.github.zemke.tippspiel2.persistence.model.enumeration.UserRole
+import com.github.zemke.tippspiel2.test.util.IntegrationTest
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(classes = [(SpringBootTippspiel2Application::class)])
+@WebAppConfiguration
+@ContextConfiguration(classes = [WebSecurityConfig::class])
+@IntegrationTest
+open class HelloRestControllerTest {
+
+    @Autowired
+    private lateinit var webApplicationContext: WebApplicationContext
+
+    private lateinit var mockMvc: MockMvc
+
+    @Before
+    fun setUp() {
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .apply<DefaultMockMvcBuilder>(springSecurity())
+                .build()
+    }
+
+    @Test
+    fun testRoleRestrictions() {
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/hellos")
+                .with(user("admin").roles(UserRole.ROLE_ADMIN.unPrefixed())))
+                .andExpect(status().isOk)
+
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/hellos")
+                .with(user("user").roles(UserRole.ROLE_USER.unPrefixed())))
+                .andExpect(status().isForbidden)
+    }
+
+}
