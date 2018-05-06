0af081b824cbe150bb658b995f28c1fb6cc79390
Zemke <flzemke@gmail.com>
2018-05-06 20:38:23 +0200

    GH-28 Impl UI

---
+++ tippspiel2-ember/src/main/webapp/app/serializers/fixture.js
@@ -0,0 +1,13 @@
+import DS from 'ember-data';
+import ApplicationJSONSerializer from './application';
+
+export default ApplicationJSONSerializer.extend(DS.EmbeddedRecordsMixin, {
+  attrs: {
+    homeTeam: {
+      deserialize: 'records'
+    },
+    awayTeam: {
+      deserialize: 'records'
+    }
+  }
+});
--- tippspiel2-ember/src/main/webapp/ember-cli-build.js
+++ tippspiel2-ember/src/main/webapp/ember-cli-build.js
@@ -4,7 +4,12 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    sassOptions: {
+      includePaths: [
+        'node_modules/bulma'
+      ],
+      extension: 'sass'
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
