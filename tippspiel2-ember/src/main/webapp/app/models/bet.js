ce47d0dcf5a384071d1f38058116033cd35853a2
Zemke <flzemke@gmail.com>
2018-05-08 00:01:30 +0200

    GH-28 Add bet model including validations

---
+++ tippspiel2-ember/src/main/webapp/app/models/bet.js
@@ -0,0 +1,22 @@
+import DS from 'ember-data';
+import {buildValidations, validator} from 'ember-cp-validations';
+
+const Validations = buildValidations({
+  goalsHomeTeamBet: [
+    validator('presence', true),
+    validator('number', {allowString: true, integer: true, gte: 0, lt: 100}),
+  ],
+  goalsAwayTeamBet: [
+    validator('presence', true),
+    validator('number', {allowString: true, integer: true, gte: 0, lt: 100}),
+  ],
+});
+
+export default DS.Model.extend(Validations, {
+  goalsHomeTeamBet: DS.attr('number'),
+  goalsAwayTeamBet: DS.attr('number'),
+  fixture: DS.belongsTo('fixture'),
+  user: DS.hasOne('user'),
+  bettingGame: DS.belongsTo('betting-game'),
+  modified: DS.attr('Date'),
+});
