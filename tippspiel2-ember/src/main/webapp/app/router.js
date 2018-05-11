42f2f7bd155032823485e0b8cd4f8b1a342d09e9
Zemke <flzemke@gmail.com>
2018-05-12 00:06:12 +0200

    GH-28 Rename games to bets

--- tippspiel2-ember/src/main/webapp/app/router.js
+++ tippspiel2-ember/src/main/webapp/app/router.js
@@ -8,7 +8,7 @@ const Router = EmberRouter.extend({
 
 Router.map(function() {
   this.route('standings');
-  this.route('games');
+  this.route('bets');
   this.route('sign-up');
   this.route('sign-in');
   this.route('me');
