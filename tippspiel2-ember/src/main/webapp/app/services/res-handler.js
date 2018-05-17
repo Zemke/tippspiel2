6e8083da09f597259627b1761eb0e675b93f169d
Zemke <flzemke@gmail.com>
2018-05-17 18:08:47 +0200

    Fix resHandler's success handler

--- tippspiel2-ember/src/main/webapp/app/services/res-handler.js
+++ tippspiel2-ember/src/main/webapp/app/services/res-handler.js
@@ -2,7 +2,7 @@ import Service from '@ember/service';
 
 export default Service.extend({
   handleSuccess(message) {
-    iziToast.success(message);
+    iziToast.success({message});
   },
   handleError(res) {
     iziToast.error({message: res.message});
