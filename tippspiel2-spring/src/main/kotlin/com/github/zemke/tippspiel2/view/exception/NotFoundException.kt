ad34608e020a7ac02f815babbf3c61d597af8236
Zemke <flzemke@gmail.com>
2018-05-22 14:28:31 +0200

    NotFoundException reason is provided manually

--- tippspiel2-spring/src/main/kotlin/com/github/zemke/tippspiel2/view/exception/NotFoundException.kt
+++ tippspiel2-spring/src/main/kotlin/com/github/zemke/tippspiel2/view/exception/NotFoundException.kt
@@ -3,7 +3,7 @@ package com.github.zemke.tippspiel2.view.exception
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
-@ResponseStatus(HttpStatus.NOT_FOUND, reason = "Resource not found.")
+@ResponseStatus(HttpStatus.NOT_FOUND)
 class NotFoundException : RuntimeException {
 
     constructor()
