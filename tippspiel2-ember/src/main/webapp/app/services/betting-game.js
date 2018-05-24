68593e75bb5e42c2c3289e16a35d53dd764010a8
Zemke <flzemke@gmail.com>
2018-05-24 17:21:00 +0200

    Import Promise from RSVP

--- tippspiel2-ember/src/main/webapp/app/services/betting-game.js
+++ tippspiel2-ember/src/main/webapp/app/services/betting-game.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 import {computed} from '@ember/object';
 import Service, {inject} from '@ember/service';
+import {Promise} from 'rsvp';
 
 export default Service.extend({
   store: inject(),
