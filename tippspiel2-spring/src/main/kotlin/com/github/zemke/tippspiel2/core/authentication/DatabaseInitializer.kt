1f3dc61f96bb160d0d1a3ee86e46e58615a74cad
Zemke <flzemke@gmail.com>
2018-05-29 01:53:13 +0200

    Reduce to only a Dev DatabaseInitializer

--- tippspiel2-spring/src/main/kotlin/com/github/zemke/tippspiel2/core/authentication/DatabaseInitializer.kt
+++
@@ -1,7 +0,0 @@
-package com.github.zemke.tippspiel2.core.authentication
-
-import org.springframework.context.ApplicationListener
-import org.springframework.context.event.ContextRefreshedEvent
-
-
-interface DatabaseInitializer : ApplicationListener<ContextRefreshedEvent>
--- tippspiel2-spring/src/main/kotlin/com/github/zemke/tippspiel2/core/authentication/DevDatabaseInitializer.kt
+++ tippspiel2-spring/src/main/kotlin/com/github/zemke/tippspiel2/core/authentication/DevDatabaseInitializer.kt
@@ -1,19 +1,20 @@
 package com.github.zemke.tippspiel2.core.authentication
 
-import com.github.zemke.tippspiel2.core.profile.Prod
+import com.github.zemke.tippspiel2.core.profile.Dev
 import com.github.zemke.tippspiel2.core.properties.AuthenticationProperties
 import com.github.zemke.tippspiel2.persistence.model.User
 import com.github.zemke.tippspiel2.persistence.model.embeddable.FullName
 import com.github.zemke.tippspiel2.persistence.repository.UserRepository
 import com.github.zemke.tippspiel2.service.RoleService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.ApplicationListener
 import org.springframework.context.event.ContextRefreshedEvent
 import org.springframework.security.crypto.bcrypt.BCrypt
 import org.springframework.stereotype.Component
 
-@Prod
+@Dev
 @Component
-class DatabaseInitializerImpl : DatabaseInitializer {
+class DevDatabaseInitializer : ApplicationListener<ContextRefreshedEvent> {
 
     @Autowired
     private lateinit var roleService: RoleService
