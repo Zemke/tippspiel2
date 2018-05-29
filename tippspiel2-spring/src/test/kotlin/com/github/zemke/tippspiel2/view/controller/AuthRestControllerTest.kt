82f56b6156ffa6e20b08a14ca28890b61dd1deb3
Zemke <flzemke@gmail.com>
2018-05-29 21:34:00 +0200

    Fix int test where user creation requires a betting game
    The DatabaseInitializer does not provide one anymore, so one has to be
    created from withing the test.

--- tippspiel2-spring/src/test/kotlin/com/github/zemke/tippspiel2/view/controller/AuthRestControllerTest.kt
+++ tippspiel2-spring/src/test/kotlin/com/github/zemke/tippspiel2/view/controller/AuthRestControllerTest.kt
@@ -2,10 +2,13 @@ package com.github.zemke.tippspiel2.view.controller
 
 import com.github.zemke.tippspiel2.SpringBootTippspiel2Application
 import com.github.zemke.tippspiel2.core.authentication.AuthenticatedUser
-import com.github.zemke.tippspiel2.service.BettingGameService
+import com.github.zemke.tippspiel2.persistence.model.BettingGame
+import com.github.zemke.tippspiel2.persistence.repository.BettingGameRepository
+import com.github.zemke.tippspiel2.persistence.repository.CompetitionRepository
 import com.github.zemke.tippspiel2.service.JsonWebTokenService
 import com.github.zemke.tippspiel2.service.UserService
 import com.github.zemke.tippspiel2.test.util.IntegrationTest
+import com.github.zemke.tippspiel2.test.util.PersistenceUtils
 import com.github.zemke.tippspiel2.view.model.AuthenticationRequestDto
 import com.github.zemke.tippspiel2.view.model.JsonWebTokenDto
 import com.github.zemke.tippspiel2.view.util.JacksonUtils
@@ -37,7 +40,10 @@ class AuthRestControllerTest {
     private lateinit var userService: UserService
 
     @Autowired
-    private lateinit var bettingGameService: BettingGameService
+    private lateinit var bettingGameRepository: BettingGameRepository
+
+    @Autowired
+    private lateinit var competitionRepository: CompetitionRepository
 
     @Autowired
     private lateinit var jsonWebTokenService: JsonWebTokenService
@@ -54,7 +60,7 @@ class AuthRestControllerTest {
         val plainPassword = "hamburger"
         val email = "Florian.Zemke@btc-ag.com"
         val requestPayload = JacksonUtils.toJson(AuthenticationRequestDto(email, plainPassword))
-        val user = userService.addUser("Florian", "Zemke", email, plainPassword, bettingGameService.findAll()[0])
+        val user = userService.addUser("Florian", "Zemke", email, plainPassword, createBettingGame())
 
         this.mockMvc.perform(post("/api/auth/")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -65,6 +71,12 @@ class AuthRestControllerTest {
                     Assert.isTrue(jsonWebTokenService.validateToken(token, AuthenticatedUser.create(user)))
                     Assert.isTrue(jsonWebTokenService.getSubjectFromToken(token) == user.email)
                 }
+    }
+
+    private fun createBettingGame(): BettingGame {
+        val bettingGame = PersistenceUtils.instantiateBettingGame()
+        return bettingGameRepository.save(
+                bettingGame.copy(competition = competitionRepository.save(bettingGame.competition)))
     }
 
 }
