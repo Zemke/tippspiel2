55da9ea3c0890e6811cf5e6ef38b5e4b6199a365
Zemke <flzemke@gmail.com>
2018-06-01 02:19:15 +0200

    GH-55 Reload and loop fix
    Closes GH-55.

--- tippspiel2-ember/src/main/webapp/app/routes/betting-games/list.js
+++ tippspiel2-ember/src/main/webapp/app/routes/betting-games/list.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    return this.get('store').findAll('betting-game');
+    return this.get('store').findAll('betting-game', {reload: true});
   }
 });
