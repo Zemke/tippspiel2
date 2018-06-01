6803d11d587479fbf4b180e6376e1b1b2d337990
Zemke <flzemke@gmail.com>
2018-06-01 02:26:16 +0200

    GH-53 Load users of betting game
    Magically Ember filters the users by betting game. Anyway, closes GH-53.

---
+++ tippspiel2-ember/src/main/webapp/app/routes/betting-games/details.js
@@ -0,0 +1,13 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+  beforeModel() {
+    this.get('store').findAll('user', {reload: true})
+  },
+  model(model) {
+    return this.get('store').findRecord('betting-game', model['betting-game_id']).then(bettingGame => {
+      bettingGame.set('users', this.get('store').query('user', {'betting-game': bettingGame.get('id')}));
+      return bettingGame;
+    });
+  }
+});
