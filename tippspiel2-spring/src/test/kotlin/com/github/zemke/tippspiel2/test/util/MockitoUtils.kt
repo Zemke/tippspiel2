a8dedb0b68813388abff42bc0534949f498e378a
Zemke <flzemke@gmail.com>
2018-06-10 17:50:04 +0200

    Allow for using Mockito#any in Kotlin null-safety

---
+++ tippspiel2-spring/src/test/kotlin/com/github/zemke/tippspiel2/test/util/MockitoUtils.kt
@@ -0,0 +1,16 @@
+package com.github.zemke.tippspiel2.test.util
+
+import org.mockito.Mockito
+
+
+object MockitoUtils {
+
+    /** [https://stackoverflow.com/a/30308199/2015430](https://stackoverflow.com/a/30308199/2015430) */
+    fun <T> anyObject(): T {
+        Mockito.any<T>()
+        return uninitialized()
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun <T> uninitialized(): T = null as T
+}
