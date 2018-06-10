a4b35c1cf8fd5e55e6b999c1081ebcc52be8926f
Zemke <flzemke@gmail.com>
2018-06-10 06:28:16 +0200

    GH-57 Query bets by betting game
    Closes GH-57.

--- tippspiel2-ember/src/main/webapp/app/routes/fixtures/bets.js
+++ tippspiel2-ember/src/main/webapp/app/routes/fixtures/bets.js
@@ -1,11 +1,15 @@
 import Route from '@ember/routing/route';
 import RSVP from 'rsvp';
+import {inject} from '@ember/service';
 
 export default Route.extend({
+  bettingGame: inject(),
   model(params) {
-    return RSVP.hash({
-      fixture: this.get('store').findRecord('fixture', params.fixture_id),
-      bets: this.get('store').query('bet', {fixture: params.fixture_id})
+    return this.get('bettingGame.currentBettingGame').then(currentBettingGame => {
+      return RSVP.hash({
+        fixture: this.get('store').findRecord('fixture', params.fixture_id),
+        bets: this.get('store').query('bet', {fixture: params.fixture_id, bettingGame: currentBettingGame.get('id')})
+      });
     });
   }
 });
