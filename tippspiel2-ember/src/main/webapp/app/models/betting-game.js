f3fbf6416db6d1b5458c2b31eb1cf427ab131fd4
Zemke <flzemke@gmail.com>
2018-06-10 11:06:09 +0200

    Remove users relation in model/betting-game.js

--- tippspiel2-ember/src/main/webapp/app/models/betting-game.js
+++ tippspiel2-ember/src/main/webapp/app/models/betting-game.js
@@ -2,7 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  users: DS.hasMany('user'),
   invitationToken: DS.attr('string'),
   competition: DS.belongsTo('competition'),
   created: DS.attr('Date')
