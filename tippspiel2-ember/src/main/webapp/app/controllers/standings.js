9f549b4965b92f12cdd8a64b76938c8a900ece1a
Marc K <morckx@gmail.com>
2018-06-14 23:44:57 +0200

    Improve sorting and rank positions in standings
    * after points, exacts bets should have highest priority
    * use the same criteria for rank positions as for sorting
    
    TODO: integrate champion bet?

--- tippspiel2-ember/src/main/webapp/app/controllers/standings.js
+++ tippspiel2-ember/src/main/webapp/app/controllers/standings.js
@@ -3,15 +3,21 @@ import {computed} from '@ember/object';
 
 export default Controller.extend({
   standingsAsTable: computed('model.standings', 'model.championBets', 'model.competition', function () {
+    function compare(s1, s2) {
+      return  (s2.get('points') - s1.get('points')
+               || s2.get('exactBets') - s1.get('exactBets')
+               || s2.get('goalDifferenceBets') - s1.get('goalDifferenceBets')
+               || s2.get('winnerBets') - s1.get('winnerBets')
+               || s1.get('missedBets') - s2.get('missedBets')
+               || s2.get('wrongBets') - s1.get('wrongBets'))
+    }
     return this.get('model.standings').toArray()
-      .sort((s1, s2) =>
-        s2.get('points') - s1.get('points')
-        || s2.get('goalDifferenceBets') - s1.get('goalDifferenceBets')
-        || s2.get('winnerBets') - s1.get('winnerBets')
-        || s1.get('missedBets') - s2.get('missedBets')
-        || s2.get('wrongBets') - s1.get('wrongBets'))
-      .map((value, index) => {
-        value.set('position', index + 1);
+      .sort((s1, s2) => compare(s1, s2))
+      .map((value, index, arr) => {
+        value.set('position', (index == 0 ? 1 :
+                               arr[index-1].get('position') +
+                               (compare(value, arr[index-1]) > 0 ? 1 : 0))
+                 );
 
         const championBet = this.get('model.championBets').find(cB => cB.get('user.id') === value.get('user.id'));
 
