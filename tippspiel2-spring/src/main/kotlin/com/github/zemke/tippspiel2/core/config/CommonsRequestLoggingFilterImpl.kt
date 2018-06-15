3fcff42c80095284295764362d4a66ad2c210941
Zemke <flzemke@gmail.com>
2018-06-16 00:28:42 +0200

    GH-67 Only log API requests and include HTTP method

--- tippspiel2-spring/src/main/kotlin/com/github/zemke/tippspiel2/core/config/CommonsRequestLoggingFilterImpl.kt
+++ tippspiel2-spring/src/main/kotlin/com/github/zemke/tippspiel2/core/config/CommonsRequestLoggingFilterImpl.kt
@@ -6,4 +6,11 @@ import javax.servlet.http.HttpServletRequest
 class CommonsRequestLoggingFilterImpl : CommonsRequestLoggingFilter() {
 
     override fun beforeRequest(request: HttpServletRequest?, message: String?) {}
+
+    @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+    override fun afterRequest(request: HttpServletRequest?, message: String?) {
+        if (request != null && request.servletPath.startsWith("/api/")) {
+            super.afterRequest(request, "method=${request.method};$message")
+        }
+    }
 }
