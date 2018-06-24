a5a925884ee5e140b556a18ec4f214195dab2c65
Zemke <flzemke@gmail.com>
2018-06-24 22:51:48 +0200

    GH-67 Fix key for value in REST logging

--- tippspiel2-spring/src/main/kotlin/com/github/zemke/tippspiel2/core/config/CommonsRequestLoggingFilterImpl.kt
+++ tippspiel2-spring/src/main/kotlin/com/github/zemke/tippspiel2/core/config/CommonsRequestLoggingFilterImpl.kt
@@ -10,7 +10,7 @@ class CommonsRequestLoggingFilterImpl : CommonsRequestLoggingFilter() {
     @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
     override fun afterRequest(request: HttpServletRequest?, message: String?) {
         if (request != null && request.servletPath.startsWith("/api/")) {
-            super.afterRequest(request, "method=${request.method};$message;method=${request.getHeader("User-Agent")}")
+            super.afterRequest(request, "method=${request.method};$message;user-agent=${request.getHeader("User-Agent")}")
         }
     }
 }
