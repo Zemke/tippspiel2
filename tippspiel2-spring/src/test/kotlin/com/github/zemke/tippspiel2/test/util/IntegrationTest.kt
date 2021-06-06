bc62c7c5599ec519a98e156474e438ed99baba32
Zemke <florian@zemke.io>
2021-06-06 21:54:29 +0200

    Fix integration tests with the power of @IntegrationTest

--- tippspiel2-spring/src/test/kotlin/com/github/zemke/tippspiel2/test/util/IntegrationTest.kt
+++ tippspiel2-spring/src/test/kotlin/com/github/zemke/tippspiel2/test/util/IntegrationTest.kt
@@ -1,8 +1,23 @@
 package com.github.zemke.tippspiel2.test.util
 
-/**
- * Hint at a test class being an integration test.
- * This currently has no further effect but the plan is to
- * split integration tests from unit tests.
- */
+import org.junit.jupiter.api.extension.ExtendWith
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.TestPropertySource
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(properties = [
+    "spring.jpa.properties.hibernate.default_schema=public",
+    "spring.datasource.hikari.driver-class-name=org.h2.Driver",
+    "spring.datasource.hikari.jdbc-url=jdbc:h2:mem:db;DB_CLOSE_DELAY=-1",
+    "spring.datasource.hikari.username=sa",
+    "spring.datasource.hikari.password=sa"
+])
+@DirtiesContext
 annotation class IntegrationTest
+
--- tippspiel2-spring/src/test/kotlin/com/github/zemke/tippspiel2/view/controller/HelloRestControllerTest.kt
+++ tippspiel2-spring/src/test/kotlin/com/github/zemke/tippspiel2/view/controller/HelloRestControllerTest.kt
@@ -1,44 +1,23 @@
 package com.github.zemke.tippspiel2.view.controller
 
-import com.github.zemke.tippspiel2.SpringBootTippspiel2Application
-import com.github.zemke.tippspiel2.core.config.WebSecurityConfig
 import com.github.zemke.tippspiel2.persistence.model.enumeration.UserRole
 import com.github.zemke.tippspiel2.test.util.IntegrationTest
-import org.junit.Before
-import org.junit.Test
-import org.junit.runner.RunWith
+import com.github.zemke.tippspiel2.view.controller.HelloRestController
+
+import org.junit.jupiter.api.Test
+
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.junit4.SpringRunner
-import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import org.springframework.web.context.WebApplicationContext
 
 
-@RunWith(SpringRunner::class)
-@WebAppConfiguration
-@ContextConfiguration(classes = [WebSecurityConfig::class])
 @IntegrationTest
 class HelloRestControllerTest {
 
     @Autowired
-    private lateinit var webApplicationContext: WebApplicationContext
-
     private lateinit var mockMvc: MockMvc
-
-    @Before
-    fun setUp() {
-        this.mockMvc = MockMvcBuilders
-                .webAppContextSetup(webApplicationContext)
-                .apply<DefaultMockMvcBuilder>(springSecurity())
-                .build()
-    }
 
     @Test
     fun testRoleRestrictions() {
@@ -54,3 +33,4 @@ class HelloRestControllerTest {
     }
 
 }
+
