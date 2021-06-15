9693dd7f2ac171aab244e52ebe4c67c1e19b1b52
Zemke <florian@zemke.io>
2021-06-15 10:17:31 +0200

    Fix app/router.js

--- tippspiel2-ember/src/main/webapp/app/router.js
+++ tippspiel2-ember/src/main/webapp/app/router.js
@@ -1,5 +1,5 @@
 import EmberRouter from '@ember/routing/router';
-import config from 'tippspiel2/config/environment';
+import config from './config/environment';
 
 export default class Router extends EmberRouter {
   location = config.locationType;
@@ -35,6 +35,4 @@ Router.map(function() {
 
   this.route('404', {path: '/*'});
 });
-
-export default Router;
 
