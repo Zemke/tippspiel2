82f6572f0e30b94fad85187a4aa6b69a88ab530d
Zemke <florian@zemke.io>
2021-06-15 10:51:59 +0200

    Remove obsolete fields from Ember model

--- tippspiel2-ember/src/main/webapp/app/models/fixture.js
+++ tippspiel2-ember/src/main/webapp/app/models/fixture.js
@@ -4,11 +4,8 @@ export default DS.Model.extend({
   date: DS.attr('Date'),
   status: DS.attr('string'),
   matchday: DS.attr('number'),
-  odds: DS.attr('number'),
   goalsHomeTeam: DS.attr('number'),
   goalsAwayTeam: DS.attr('number'),
   homeTeam: DS.belongsTo('team'),
   awayTeam: DS.belongsTo('team'),
-  competition: DS.belongsTo('competition'),
-  manual: DS.attr('boolean'),
 });
--- tippspiel2-ember/src/main/webapp/app/models/team.js
+++ tippspiel2-ember/src/main/webapp/app/models/team.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  id: DS.attr('number'),
   name: DS.attr('string'),
-  squadMarketValue: DS.attr('string'),
   competition: DS.belongsTo('competition'),
 });
